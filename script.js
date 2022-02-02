7a529bd5f3a589cf8c2e3819670c05aa180cf163
Thea Villanueva <tei.villanueva@gmail.com>
2022-02-02 08:46:37 -0800

    Add Dojo Weather

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let clickCity = document.querySelector("#city");
+console.log(loading);
+
+let cookies = document.querySelector("#cookies")
+console.log(cookies)
+
+function loading(){
+    alert("Loading weather report...")
+}
+
+function removeCookie(){
+    cookies.remove();
+
+}
+
+
+
